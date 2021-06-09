f8301d94af0f7d9106ecefd2cbba363351d68a4b
evilpaper <pellburt@gmail.com>
2021-06-09 16:22:09 +0200

    Add delete todo button

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Jobs() {
+  const [jobs, setJobs] = useState([]);
   useEffect(() => {
     fetch("https://react-tutorial-demo.firebaseio.com/users.json")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        setJobs(data);
       });
   }, []);
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,6 +9,16 @@ export default function List({ items }: { items: Todo[] }) {
           <li key={index} className="todo__item">
             <input className="todo__checkbox" type="checkbox" />
             <p>{item.text}</p>
+            <button>
+              <svg width="20" height="20" fill="currentColor">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                />
+              </svg>
+              Delete
+            </button>
           </li>
         );
       })}
