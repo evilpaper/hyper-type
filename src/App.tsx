270cb187b1d798b73bdac7788b1526ec6d467c1d
evilpaper <pellburt@gmail.com>
2021-06-12 13:10:26 +0200

    Add react router to handle routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
-
 import Modal from "./components/Modal";
-
 import { Greetings } from "./components/Greetings";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
-import Jobs from "./components/Jobs";
-
 import useOutsideClick from "./hooks/useOutsideClick";
 import "./App.css";
 import { Todo } from "./types";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Jobs from "./components/Jobs";
 
 function App() {
   const [todos, setTodos] = useState<Array<Todo>>([]);
@@ -31,23 +29,30 @@ function App() {
   }
 
   return (
-    <div className="app">
-      {isModalOpen && (
-        <Modal onOutsideClick={ref}>
-          <p>Hey, I'm a modal. Click anywhere outside of me to close.</p>
-        </Modal>
-      )}
-      <section className="header">
-        <Greetings message="Hyper Type" />
-        <button onClick={() => setIsModalOpen(true)}>Sign in</button>
-      </section>
-      <section className="main">
-        <article>
-          <AddTodo handleAddTodo={handleAddTodo} />
-          <TodoList items={todos} deleteTodo={deletetodo} />
-        </article>
-      </section>
-    </div>
+    <Router>
+      <div className="app">
+        {isModalOpen && (
+          <Modal onOutsideClick={ref}>
+            <p>Hey, I'm a modal. Click anywhere outside of me to close.</p>
+          </Modal>
+        )}
+        <section className="header">
+          <Greetings message="Hyper Type" />
+          <button onClick={() => setIsModalOpen(true)}>Sign in</button>
+        </section>
+        <section className="main">
+          <article>
+            <AddTodo handleAddTodo={handleAddTodo} />
+            <TodoList items={todos} deleteTodo={deletetodo} />
+          </article>
+        </section>
+      </div>
+      <Switch>
+        <Route path="/jobs">
+          <Jobs />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
