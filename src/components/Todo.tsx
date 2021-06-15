f03e72af6e8b98a54f7b1142534d0a28b9af8c12
evilpaper <pellburt@gmail.com>
2021-06-15 07:05:59 +0200

    Refactor todo item to own component

---
+++ src/components/Todo.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Todo } from "../types";
+
+export default function TodoItem({
+  item,
+  onDeleteTodo,
+}: {
+  item: Todo;
+  onDeleteTodo: (item: Todo) => void;
+}) {
+  return (
+    <li key={item.id} className="todo__item">
+      <div className="todo__main">
+        <input className="todo__checkbox" type="checkbox" />
+        <p>{item.text}</p>
+      </div>
+      <button className="todo__delete" onClick={() => onDeleteTodo(item)}>
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="2.5"
+            width="24.7487"
+            height="3.53553"
+            rx="1.76777"
+            transform="rotate(45 2.5 0)"
+          />
+          <rect
+            x="20"
+            y="2.5"
+            width="24.7487"
+            height="3.53553"
+            rx="1.76777"
+            transform="rotate(135 20 2.5)"
+          />
+        </svg>
+      </button>
+    </li>
+  );
+}
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React, { FormEvent, useState, useEffect } from "react";
 import { Todo } from "../types";
+import TodoItem from "./Todo";
 
 export default function List({
   items,
@@ -43,43 +44,8 @@ export default function List({
         />
       </form>
       <ul>
-        {currentList.map((item, index) => {
-          return (
-            <li key={index} className="todo__item">
-              <div className="todo__main">
-                <input className="todo__checkbox" type="checkbox" />
-                <p>{item.text}</p>
-              </div>
-              <button
-                className="todo__delete"
-                onClick={() => onDeleteTodo(item)}
-              >
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    x="2.5"
-                    width="24.7487"
-                    height="3.53553"
-                    rx="1.76777"
-                    transform="rotate(45 2.5 0)"
-                  />
-                  <rect
-                    x="20"
-                    y="2.5"
-                    width="24.7487"
-                    height="3.53553"
-                    rx="1.76777"
-                    transform="rotate(135 20 2.5)"
-                  />
-                </svg>
-              </button>
-            </li>
-          );
+        {currentList.map((item) => {
+          return <TodoItem item={item} onDeleteTodo={onDeleteTodo} />;
         })}
       </ul>
     </>
