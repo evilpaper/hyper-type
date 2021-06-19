7ff10a25f21b863b08b2d1ba50cf16225b6a0b58
evilpaper <pellburt@gmail.com>
2021-06-19 09:02:05 +0200

    Retrive countries fron trevor blades countries api

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Countries() {
+  useEffect(() => {
+    fetch("https://countries.trevorblades.com/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `
+        query {
+            countries {
+              name
+              capital
+            }
+          }
+        `,
+      }),
+    }).then((response) => response.json().then((data) => console.log(data)));
+  });
+
   return <h1>Hello, I'm the countries component</h1>;
 }
