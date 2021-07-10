b3da56f2bb442dde7216ca7505d1c0b21e0cc196
Pelle Lundgren <pellburt@gmail.com>
2021-07-10 07:43:17 +0200

    Move modal styles to styled components

--- src/common/Modal/Modal.tsx
+++ src/common/Modal/Modal.tsx
@@ -1,4 +1,29 @@
 import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+const ScreenOverlay = styled.div`
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: hsla(255, 8%, 8%, 0.2);
+`;
+
+const StyledModal = styled.div`
+  width: 14em;
+  line-height: 1.5;
+  text-align: center;
+  color: black;
+  background-color: white;
+  padding: 2em;
+  border-radius: 1em;
+  box-shadow: 2px 3px 12px hsl(255, 8%, 38%, 0.2),
+    7px 10px 12px hsl(255, 10%, 38%, 0.15),
+    5px 14px 12px hsl(255, 12%, 38%, 0.1),
+    12px 16px 12px hsl(255, 14%, 38%, 0.05);
+`;
 
 export default function Modal({
   children,
@@ -8,10 +33,8 @@ export default function Modal({
   onOutsideClick: any;
 }) {
   return (
-    <div className="container">
-      <div ref={onOutsideClick} className="modal">
-        {children}
-      </div>
-    </div>
+    <ScreenOverlay>
+      <StyledModal ref={onOutsideClick}>{children}</StyledModal>
+    </ScreenOverlay>
   );
 }
